552476ce350c46006f7fcecfb04766f6d35e0900
Giga <gigabyte5671@gmail.com>
2023-07-10 10:48:46 +1200

    Create more unit tests

---
+++ tests/ignored.spec.ts
@@ -0,0 +1,41 @@
+/*
+ * Verify that `:root`, `*`, etc are ignored correctly.
+ */
+
+import { test, expect } from '@playwright/test';
+import { cssToHtml } from '../src/index';
+
+const css = `
+@import url('https://example.com/example.css');
+:root {
+	background: #000;
+}
+* {
+	content: 'a';
+	box-sizing: border-box;
+}
+@media screen and (max-width: 200px) {}
+div {
+	content: 'a';
+}
+div:hover {
+	content: 'b';
+}
+`;
+
+test('Ignored', async ({ page }) => {
+	await page.addScriptTag({ path: './tests/GeneratorScript.js' });
+
+	const result = await page.evaluate(async (css) => {
+		document.body = cssToHtml(css);
+
+		const element = document.body.querySelector('div');
+		return element
+			&& element.innerHTML === 'a'
+			&& element.previousSibling === null
+			&& element.nextSibling === null;
+	}, css);
+
+	expect(result).toBeDefined();
+	expect(result).toBe(true);
+});
---
+++ tests/selector.spec.ts
@@ -0,0 +1,40 @@
+/*
+ * Verify that complex selectors will be parsed correctly.
+ */
+
+import { test, expect } from '@playwright/test';
+import { cssToHtml } from '../src/index';
+
+const css = `
+#cat + .mouse  >span.flea+i {
+	padding: 10px;
+	background-color: red;
+}
+nav    a#logo.icon> img {
+	content: 'https://example.com/image';
+	display: block;
+}
+nav>a#logo.icon > img {
+	content: 'https://example.com/image2';
+}
+`;
+
+const html = `<body xmlns="http://www.w3.org/1999/xhtml"><div id="cat"></div><div class="mouse"><span class="flea"></span><i></i></div><nav><a class="icon" id="logo"><img src="https://example.com/image2" /></a></nav></body>`;
+
+test('Selector', async ({ page }) => {
+	await page.addScriptTag({ path: './tests/GeneratorScript.js' });
+
+	const result = await page.evaluate(async ([css, html]) => {
+		document.body = cssToHtml(css);
+		const styleElement = document.createElement('style');
+		styleElement.innerText = css;
+		document.head.append(styleElement);
+
+		const xml = new XMLSerializer().serializeToString(document.body);
+
+		return xml.trim() === html.trim();
+	}, [css, html]);
+
+	expect(result).toBeDefined();
+	expect(result).toBe(true);
+});
