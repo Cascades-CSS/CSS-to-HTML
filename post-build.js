3cba06c39963fda8e2de2ee199ab1559b7b53184
Giga <gigabyte5671@gmail.com>
2023-09-11 13:50:56 +1200

    Remove deprecated post-build script

--- post-build.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-let script = fs.readFileSync('dist/Generator.js', { encoding: 'utf-8' });
-script = script.replace('export ', '');
-script = script.replace(/\/\/# sourceMappingURL=[a-z\.]+/i, '');
-
-fs.writeFileSync('dist/Generator.script.js', script, { encoding: 'utf-8' });
-
-console.log('Done.');
